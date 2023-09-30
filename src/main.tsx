fa28aa1381d0174c1cc45d2503be2d17e164c85d
Mohamed <mxsameh@gmail.com>
2023-09-30 23:11:10 +0300

    added down page

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import ReactDOM from "react-dom/client";
 import "./assets/index.css";
 import Home from "./pages/Home";
 import PopupProvider from "./context/PopupContext";
+import Down from "./pages/Down";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <PopupProvider>
-        <Home />
-    </PopupProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<Down />);
 
-// <React.StrictMode>
-//   <Home/>
-// </React.StrictMode>
+// <PopupProvider>
+//     <Home />
+// </PopupProvider>
---
+++ src/pages/Down/index.tsx
@@ -0,0 +1,23 @@
+import styles from "./styles.module.scss";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
+const Down = () => {
+    return (
+        <main>
+            <Logo className={styles.logo} />
+            <a href="https://maisonpyramide.com/" target="_blank" className={styles.company}>
+                a <b>MAISON PYRAMIDE</b> company
+            </a>
+            <h1 className={styles.title}> coming soon</h1>
+            <ul className={styles.socials}>
+                <li>
+                    <a href="https://www.instagram.com/egoandeast/">instagram</a>
+                </li>
+                <li>
+                    <a href="">linkedin</a>
+                </li>
+            </ul>
+        </main>
+    );
+};
+export default Down;
