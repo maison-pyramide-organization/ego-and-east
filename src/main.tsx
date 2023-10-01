0a9f5cbb14a7049fe505ccf82487da6c737a7a9c
Mohamed <mxsameh@gmail.com>
2023-10-01 11:35:20 +0300

    testing prod

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
 import "./assets/index.css";
-// import Home from "./pages/Home";
-// import PopupProvider from "./context/PopupContext";
-import Down from "./pages/Down";
+import Home from "./pages/Home";
+import PopupProvider from "./context/PopupContext";
+// import Down from "./pages/Down";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<Down />);
-
-// <PopupProvider>
-//     <Home />
-// </PopupProvider>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <PopupProvider>
+        <Home />
+    </PopupProvider>
+);
