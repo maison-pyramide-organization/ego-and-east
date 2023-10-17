0c1861f0928a9b8629f576233e4f49d250894a6e
Mohamed <mxsameh@gmail.com>
2023-10-17 12:48:07 +0300

    added slideshow

---
+++ src/components/Hero/SlideShow/index.tsx
@@ -0,0 +1,29 @@
+import styles from "./styles.module.scss";
+import slideshowImages from "../../../data/slideshow";
+
+const SlideShow = () => {
+    let i = 0;
+    const changeImage = () => {
+        const time = 5000;
+        const images = slideshowImages;
+        const slide = document.querySelector("#slide img") as HTMLImageElement;
+        slide.src = images[i];
+        if (i < images.length - 1) i++;
+        else i = 0;
+
+        setTimeout(changeImage, time);
+    };
+    window.onload = changeImage;
+
+    return (
+        <>
+            <div className={styles.slideshow}>
+                <div id="slide" className={styles.slide}>
+                    <img className={styles.slide} alt="image" />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SlideShow;
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,15 +1,10 @@
 import styles from "./styles.module.scss";
-import image from "../../assets/e&e-slideshow.gif";
 import { ReactComponent as MpCompanyLogo } from "../../assets/MP Company logo.svg";
+import SlideShow from "./SlideShow";
 const Hero = () => {
     return (
         <main className={styles.hero}>
-            <div className={styles.heroWraper}>
-                <div className={styles.imageContainer}>
-                    <img src={image} className={styles.image} alt="EGO & EAST" />
-                </div>
-                {/* <h1 className={styles.title}>Zeyne x YUNG & Miu Miu</h1> */}
-            </div>
+            <SlideShow />
             <MpCompanyLogo className={styles.companyLogo} />
             <h3 className={styles.info}>
                 We connect top global luxury, beauty and fashion brands with the most compelling{" "}
