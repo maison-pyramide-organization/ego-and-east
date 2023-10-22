3e6fe7190eb1d55e473c88cf5e34f55ecb47094e
Daniel Wassiely <73672@must.edu.eg>
2023-10-23 01:24:31 +0300

    adding logo transition && integrate instagram

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,33 +1,87 @@
-import { useEffect } from "react";
-import images from "../../data/gallery";
+import { useEffect, useState } from "react";
+// import images from "../../data/gallery";
 import styles from "./styles.module.scss";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ReactComponent as IgIcon } from "../../assets/Icons/instagram.svg";
 import animate from "./animation";
 
-const ig_url = "https://www.instagram.com/egoandeast/";
-
+export interface InstaItem {
+    permalink: string;
+    mediaUrl: string;
+    mediaType: string;
+}
 const Gallery = () => {
+    const ig_url = "https://www.instagram.com/egoandeast/";
+    const accessToken = "IGQWRQU2x1azV5OXlvMXBVVFluU3Y0SGlqTlZAUUDdXZAXBPTGJWcFZAfSV9tMndMMDFMaERNSXVtWDVxTmtaLVBIbkp0bnNHOEkxOGM2NVh3aVA1SWVIYW5wVTVvcDNYRU1WTnJsYkt2dXFPaVowTUMzWlpXMTE5ZAUkZD"
+    const userId = "23949713848007342"
+    const instaUrl = `https://graph.instagram.com/${userId}/media?access_token=${accessToken}`
+
+    const [instaItems, setInstaItems] = useState<InstaItem[]>([])
+  
     useEffect(() => {
+       
+        const fetchMedia = async (id: string) => {
+            const mediaUrl = `https://graph.instagram.com/${id}?access_token=${accessToken}&fields=media_url,media_type,permalink`
+            const res = await fetch(mediaUrl);
+            const json = (await res.json());
+            
+            const instaItem = {
+                permalink: json.permalink,
+                mediaUrl: json.media_url,
+                mediaType: json.media_type,
+            }
+            return instaItem
+        }
+        const doFetch = async () => {
+            if (!userId || !accessToken) {
+                console.log("userId or accessToken is undefined: ", { userId, accessToken });
+                return;
+            }
+
+            const res = await fetch(instaUrl);
+            const json = (await res.json()).data;
+            console.log(json);
+
+            const fetchedItems: InstaItem[] = [];
+
+            for (let i = 0; i < json.length && i < 25; i++) {
+                const item = json[i];
+                const itemId = item.id;
+
+                const instaItem = await fetchMedia(itemId);
+                fetchedItems.push(instaItem);
+            }
+            setInstaItems(fetchedItems);
+        }
+        doFetch();
+    }, [userId, accessToken, instaUrl]);
+    
+    useEffect(() => { 
         gsap.registerPlugin(ScrollTrigger);
         animate();
-    }, []);
-
+    }, [instaItems]);
+    // console.log("instaItems",instaItems)
     return (
         <section className="section">
             <div className={styles.galleryContainer}>
                 <div id="gallery" className={styles.gallery}>
                     {/* IMAGES  */}
-                    {images.map((image) => (
-                        <div
-                            className={styles.imageWraper}
-                            key={image.id}
-                            style={{ backgroundImage: `url(${image.image})` }}
-                        >
-                            <IgIcon className={styles.igIcon} />
-                        </div>
-                    ))}
+                    {instaItems.map((image) => {
+                        if(image.mediaType !== "VIDEO"){
+                      
+                     return( <a target="_blank" href={image.permalink}>
+                            <div
+
+                                className={styles.imageWraper}
+                                key={image.permalink}
+                                style={{ backgroundImage: `url(${image.mediaUrl})` }}
+                            >
+                                <IgIcon className={styles.igIcon} />
+                            </div>
+                        </a>)
+                    }}
+                    )}
                 </div>
             </div>
             <a href={ig_url} target="_default" className={styles.igLink}>
--- src/pages/Loader/index.tsx
+++ src/pages/Loader/index.tsx
@@ -1,14 +1,37 @@
 import loaderStyle from "./LoaderStyle.module.scss";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import {
+  TransitionGroup,
+  CSSTransition,
+} from "react-transition-group";
+import React from "react";
 
 const LoaderComponent = ({ isLoading }: { isLoading: boolean }) => {
+  const [hideWhole, setHideWhole] = React.useState(false);
+  if (!isLoading) {
+    setTimeout(() => {
+      setHideWhole(true);
+    }, 500);
+  }
+
   return (
     <div>
-      {isLoading && (
-        <div className={loaderStyle.main}>
-          <Logo className={loaderStyle.logo} />
-        </div>
-      )}
+      <TransitionGroup>
+        {!hideWhole && (
+          <CSSTransition key={2} timeout={500} classNames={"item"}>
+            <div className={loaderStyle.main}>
+              <h1 className={loaderStyle.h1}>
+                {/* MAISON
+                <br />
+                PYRAMIDE */}
+               <Logo className={loaderStyle.logo}/>
+                {/* <Image src={logo} unoptimized={true} className={loaderStyle.logo} layout="responsive"></Image> */}
+              </h1>
+              <br />
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </div>
   );
 };
