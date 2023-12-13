8a256c5774f6fb67a3e197f6a219c9ce3587cdc7
Mohamed Sameh <mxsameh@gmail.com>
2023-12-13 15:15:00 +0200

    updated mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,28 +8,29 @@ import Popup from "../../components/Popup";
 import Services from "../../components/Services";
 import styles from "./styles.module.scss";
 import LoaderComponent from "../Loader";
+import Gallery from "../../components/Gallery";
 
 const Home = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 1000);
-    }, []);
-    return (
-        <>
-            <LoaderComponent isLoading={isLoading} />
-            <Header />
-            <main className={styles.page}>
-                <Hero />
-                <Services />
-                <About />
-                {/* <Gallery /> */}
-                <Music />
-            </main>
-            <Footer />
-            <Popup />
-        </>
-    );
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+  return (
+    <>
+      <LoaderComponent isLoading={isLoading} />
+      <Header />
+      <main className={styles.page}>
+        <Hero />
+        <Services />
+        <About />
+        <Gallery />
+        <Music />
+      </main>
+      <Footer />
+      <Popup />
+    </>
+  );
 };
 export default Home;
