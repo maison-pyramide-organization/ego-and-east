a73510f6e0d34f0a7417b51facd84868419eb7a0
Mohamed Sameh <mxsameh@gmail.com>
2023-12-15 19:30:00 +0200

    fixed some styles

--- src/components/Music/Playlist/index.tsx
+++ src/components/Music/Playlist/index.tsx
@@ -5,23 +5,31 @@ import { ReactComponent as PlayIcon } from "../../../assets/Icons/play.svg";
 import classNames from "classnames";
 
 interface Iprops {
-    playlist: Iplaylist;
+  playlist: Iplaylist;
 }
 
 const Playlist = (props: Iprops) => {
-    const { playlist } = props;
-    const playBtnClass =
-        playlist.playBtn == "black"
-            ? classNames(styles.playBtn, styles.blackBtn)
-            : classNames(styles.playBtn, styles.whiteBtn);
-    return (
-        <div className={styles.playlist}>
-            <img className={styles.playlistImage} src={playlist.image} alt="" />
-            <SpotifyIcon className={styles.spotifyIcon} />
-            <a target="_blank" href={playlist.link} className={playBtnClass} title="playlist">
-                <PlayIcon className={styles.playIcon} />
-            </a>
-        </div>
-    );
+  const { playlist } = props;
+  const playBtnClass =
+    playlist.playBtn == "black"
+      ? classNames(styles.playBtn, styles.blackBtn)
+      : classNames(styles.playBtn, styles.whiteBtn);
+
+  return (
+    <div className={styles.playlist}>
+      <img className={styles.playlistImage} src={playlist.image} alt="" />
+      <SpotifyIcon className={styles.spotifyIcon} />
+      <a
+        target="_blank"
+        href={playlist.link}
+        className={playBtnClass}
+        title="playlist"
+      >
+        <PlayIcon className={styles.playIcon} />
+      </a>
+    </div>
+  );
+
 };
+
 export default Playlist;
