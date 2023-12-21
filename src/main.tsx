41474e7509777e0fd1b982a34edb08fc00a60ccf
Mohamed <mxsameh@gmail.com>
2023-12-21 19:06:50 +0200

    added production branch

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from "react-dom/client";
 import "./assets/index.css";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import PopupProvider from "./context/PopupContext";
-import Down from "./pages/Down";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <PopupProvider>
-        {/* <Home /> */}
-        <Down/>
+        <Home />
     </PopupProvider>
 );
