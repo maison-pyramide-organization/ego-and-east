fe8bb6b9eb83561c25c450891761b374efe630e4
Mohamed <mxsameh@gmail.com>
2024-01-07 13:21:38 +0200

    fixed mobile popup

--- src/components/Popup/Form/index.tsx
+++ src/components/Popup/Form/index.tsx
@@ -1,84 +1,78 @@
 import classNames from "classnames";
 import styles from "./styles.module.scss";
 import { field } from "../../../types/fields";
-import {
-  becomeATalentFields,
-  bookAtalentFields,
-  contactFields,
-} from "../../../data/popups";
+import { becomeATalentFields, bookAtalentFields, contactFields } from "../../../data/popups";
 import sendEmail from "../utils/sendEmail";
+import { useRef } from "react";
 
 interface Iprops {
-  popup: string;
-  viewSentMessage: () => void;
+    popup: string;
+    viewSentMessage: () => void;
 }
 
 const Form = (props: Iprops) => {
-  const { popup, viewSentMessage } = props;
-  let message = "";
-  let inputs: field[];
+    const { popup, viewSentMessage } = props;
+    const formRef = useRef(null);
+    let message = "";
+    let inputs: field[];
 
-  switch (popup) {
-    case "book a talent":
-      inputs = bookAtalentFields;
-      message = `What kind of talent are you looking for? \n
+    switch (popup) {
+        case "book a talent":
+            inputs = bookAtalentFields;
+            message = `What kind of talent are you looking for? \n
 (max 100 characters)`;
-      break;
-    case "become a talent":
-      inputs = becomeATalentFields;
-      message = `Tell us about yourself – and your goals! \n
+            break;
+        case "become a talent":
+            inputs = becomeATalentFields;
+            message = `Tell us about yourself – and your goals! \n
 (max 100 characters)`;
-      break;
-    case "get in contact":
-      inputs = contactFields;
-      message = `Tell us more... \n
+            break;
+        case "get in contact":
+            inputs = contactFields;
+            message = `Tell us more... \n
 (max 100 characters)`;
-      break;
-  }
+            break;
+    }
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    // GET FORM DATA
-    const form = document.getElementById("form") as any;
-    const formData: any = {
-      requestType: form.requestType.value,
-      message: form.message.value,
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        // GET FORM DATA
+        const form = formRef.current! as any;
+        const formData: any = {
+            requestType: form.requestType.value,
+            message: form.message.value,
+        };
+        inputs.forEach((input: any) => {
+            const name = input.name;
+            const value = form[name].value;
+            formData[name] = value;
+        });
+
+        sendEmail(formData);
+        viewSentMessage();
+
+        form.reset();
     };
-    inputs.forEach((input: any) => {
-      const name = input.name;
-      const value = form[name].value;
-      formData[name] = value;
-    });
 
-    sendEmail(formData);
-    viewSentMessage();
+    return (
+        <form className={classNames(styles.form)} onSubmit={handleSubmit} ref={formRef}>
+            <div className={styles.inputsWraper}>
+                <input type="hidden" value={popup} id="requestType" hidden />
+                {inputs!.map((input) => (
+                    <div className={styles.formInput} key={input.name}>
+                        <label htmlFor={input.name}></label>
+                        <input placeholder={input.name} type="text" id={input.name} />
+                    </div>
+                ))}
+            </div>
 
-    form.reset();
-  };
-
-  return (
-    <form className={classNames(styles.form)} onSubmit={handleSubmit} id="form">
-      <div className={styles.inputsWraper}>
-        <input type="hidden" value={popup} id="requestType" hidden />
-        {inputs!.map((input) => (
-          <div className={styles.formInput} key={input.name}>
-            <label htmlFor={input.name}></label>
-            <input placeholder={input.name} type="text" id={input.name} />
-          </div>
-        ))}
-      </div>
-
-      <div className={styles.messageContainer}>
-        <textarea
-          placeholder={message}
-          id="message"
-          className={styles.message}
-        />
-        <button type="submit" className={styles.sendBtn}>
-          send
-        </button>
-      </div>
-    </form>
-  );
+            <div className={styles.messageContainer}>
+                <textarea placeholder={message} id="message" className={styles.message} />
+                <button type="submit" className={styles.sendBtn}>
+                    send
+                </button>
+            </div>
+        </form>
+    );
 };
 export default Form;
--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,53 +1,57 @@
 import styles from "./styles.module.scss";
 import classNames from "classnames";
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { IPopupContext, PopupContext } from "../../context/PopupContext";
 import Header from "./Header";
 import Form from "./Form";
 
 const Popup = () => {
-  const { popup, setPopup } = useContext(PopupContext) as IPopupContext;
-  const [isSent, setIsSent] = useState(false);
+    const { popup, setPopup } = useContext(PopupContext) as IPopupContext;
+    const [isSent, setIsSent] = useState(false);
+    const popupRef = useRef(null);
 
-  if (!popup) return null;
-  document.body.classList.add("disable-scroll");
+    if (!popup) return null;
+    document.body.classList.add("disable-scroll");
 
-  const closePopup = () => {
-    document.body.classList.remove("disable-scroll");
-    setPopup(null);
-    setIsSent(false);
-  };
+    const closePopup = () => {
+        document.body.classList.remove("disable-scroll");
+        setPopup(null);
+        setIsSent(false);
+    };
 
-  const bodyClasses = isSent
-    ? classNames(styles.popupBody, styles.hide)
-    : classNames(styles.popupBody);
+    const bodyClasses = isSent
+        ? classNames(styles.popupBody, styles.hide)
+        : classNames(styles.popupBody);
 
-  const sentContainerClasses = isSent
-    ? classNames(styles.sent_container)
-    : classNames(styles.sent_container, styles.hide);
+    const sentContainerClasses = isSent
+        ? classNames(styles.sent_container)
+        : classNames(styles.sent_container, styles.hide);
 
-  const viewSentMessage = () => {
-    setIsSent(true);
-    setTimeout(closePopup, 2500);
-  };
+    const viewSentMessage = () => {
+        const popupEl = popupRef.current! as HTMLElement;
+        popupEl.scrollTop = 0
+        popupEl.style.overflow = "hidden";
+        setIsSent(true);
+        setTimeout(closePopup, 2500);
+    };
 
-  return (
-    <div className={styles.overlay}>
-      <div className={classNames(styles.popup, "disable-scroll")}>
-        <Header close={closePopup} />
+    return (
+        <div className={styles.overlay}>
+            <div className={classNames(styles.popup, "disable-scroll")} ref={popupRef}>
+                <Header close={closePopup} />
 
-        <main className={bodyClasses}>
-          <h2 className={styles.popupTitle}>{popup}</h2>
-          <Form popup={popup} viewSentMessage={viewSentMessage} />
-        </main>
+                <main className={bodyClasses}>
+                    <h2 className={styles.popupTitle}>{popup}</h2>
+                    <Form popup={popup} viewSentMessage={viewSentMessage} />
+                </main>
 
-        {/* MESSAGE SENT */}
-        <div className={sentContainerClasses}>
-          <h3 className={styles.sent_text}>We’ve received your message!</h3>
+                {/* MESSAGE SENT */}
+                <div className={sentContainerClasses}>
+                    <h3 className={styles.sent_text}>We’ve received your message!</h3>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Popup;
