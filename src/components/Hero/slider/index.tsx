3b8e036282fa5e283063b022c35e9778402686d0
Mohamed Sameh <mxsameh@gmail.com>
2024-01-10 14:47:44 +0200

    fixed css

--- src/components/Hero/slider/index.tsx
+++ src/components/Hero/slider/index.tsx
@@ -5,29 +5,33 @@ import styles from "./styles.module.scss";
 import { Autoplay } from "swiper/modules";
 
 const Slider = () => {
-    return (
-            <Swiper
-                modules={[Autoplay]}
-                loop={true}
-                autoplay={{ delay: 8000 }}
-                slidesPerView={1}
-                breakpoints={{
-                    1024: {
-                        slidesPerView: 3,
-                        spaceBetween: 64,
-                    },
-                }}
-            >
-                {sliderImages.map((slide, i) => (
-                    <SwiperSlide className={styles.slide} key={i}>
-                        <div className={styles.slide_img_wrapper}>
-                            <img src={slide.img} className={styles.slide_img} alt={slide.title} />
-                        </div>
-                        <span className={styles.slide_title}>{slide.title}</span>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-    );
+  return (
+    <Swiper
+      modules={[Autoplay]}
+      loop={true}
+      autoplay={{ delay: 8000 }}
+      slidesPerView={1}
+      breakpoints={{
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 64,
+        },
+      }}
+    >
+      {sliderImages.map((slide, i) => (
+        <SwiperSlide className={styles.slide} key={i}>
+          <div className={styles.slide_img_wrapper}>
+            <img
+              src={slide.img}
+              className={styles.slide_img}
+              alt={slide.title}
+            />
+          </div>
+          <span className={styles.slide_title}>{slide.title}</span>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default Slider;
