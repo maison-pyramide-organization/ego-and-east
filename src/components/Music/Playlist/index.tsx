214ac63ce80014750f46f828a170fedc63da5ea5
Mohamed <mxsameh@gmail.com>
2024-09-03 00:39:06 +0300

    sync

--- src/components/Music/Playlist/index.tsx
+++ src/components/Music/Playlist/index.tsx
@@ -5,32 +5,26 @@ import { ReactComponent as PlayIcon } from "../../../assets/Icons/play.svg";
 import classNames from "classnames";
 
 interface Iprops {
-  playlist: Iplaylist;
+    playlist: Iplaylist;
 }
 
 const Playlist = (props: Iprops) => {
-  const { playlist } = props;
-  console.log("p", playlist.image);
+    const { playlist } = props;
 
-  const playBtnClass =
-    playlist.button_color == "black"
-      ? classNames(styles.playBtn, styles.blackBtn)
-      : classNames(styles.playBtn, styles.whiteBtn);
+    const playBtnClass =
+        playlist.button_color == "black"
+            ? classNames(styles.playBtn, styles.blackBtn)
+            : classNames(styles.playBtn, styles.whiteBtn);
 
-  return (
-    <div className={styles.playlist}>
-      <img className={styles.playlistImage} src={playlist.image?.asset.url} alt="" />
-      <SpotifyIcon className={styles.spotifyIcon} />
-      <a
-        target="_blank"
-        href={playlist.link}
-        className={playBtnClass}
-        title="playlist"
-      >
-        <PlayIcon className={styles.playIcon} />
-      </a>
-    </div>
-  );
+    return (
+        <div className={styles.playlist}>
+            <img className={styles.playlistImage} src={playlist.image?.asset.url} alt="" />
+            <SpotifyIcon className={styles.spotifyIcon} />
+            <a target="_blank" href={playlist.link} className={playBtnClass} title="playlist">
+                <PlayIcon className={styles.playIcon} />
+            </a>
+        </div>
+    );
 };
 
 export default Playlist;
--- src/components/Music/index.tsx
+++ src/components/Music/index.tsx
@@ -6,25 +6,26 @@ import getPlaylists from "../../services/api/getPlaylists";
 import { Iplaylist } from "../../types/playlists";
 
 const Music = () => {
-  const [playlists, setPlaylists] = useState<Iplaylist[] | null>(null);
+    const [playlists, setPlaylists] = useState<Iplaylist[] | null>(null);
 
-  useEffect(() => {
-    getPlaylists().then((pls) => {
-      setPlaylists(pls);
-    });
-  }, []);
+    useEffect(() => {
+        getPlaylists().then((pls) => {
+            const sortedPlaylists = pls.sort((a, b) => a.id - b.id);
+            setPlaylists(sortedPlaylists);
+        });
+    }, []);
 
-  return (
-    <section className={classNames(styles.music, "section")}>
-      <h1 className={classNames("title")}>hit shuffle and enjoy</h1>
-      <ul className={styles.musicList}>
-        {playlists?.map((playlist) => (
-          <Fragment key={playlist.id}>
-            <Playlist playlist={playlist} />
-          </Fragment>
-        ))}
-      </ul>
-    </section>
-  );
+    return (
+        <section className={classNames(styles.music, "section")}>
+            <h1 className={classNames("title")}>hit shuffle and enjoy</h1>
+            <ul className={styles.musicList}>
+                {playlists?.map((playlist) => (
+                    <Fragment key={playlist.id}>
+                        <Playlist playlist={playlist} />
+                    </Fragment>
+                ))}
+            </ul>
+        </section>
+    );
 };
 export default Music;
