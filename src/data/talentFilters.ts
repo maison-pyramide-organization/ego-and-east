7e43e4504945f6f398cf511b6c9156143396a958
Mohamed Sameh <mxsameh@gmail.com>
2024-09-20 16:53:25 +0300

    sync

--- src/data/talentFilters.ts
+++ src/data/talentFilters.ts
@@ -2,7 +2,7 @@ const filters = [
   { name: "all", category: "all" },
   { name: "actors", category: "actor" },
   { name: "creatives", category: "creative" },
-  { name: "musician", category: "musician" },
+  { name: "musicians", category: "musician" },
 ];
 
 export default filters;
