7bf3877ebc383d9f2bc9e9ced8ca62f67468c1b5
maison-pyramide-co <137396278+maison-pyramide-co@users.noreply.github.com>
2024-09-24 11:42:45 +0300

    Update index.tsx

--- src/components/Header/Left Nav/index.tsx
+++ src/components/Header/Left Nav/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { useEffect } from "react";
 import styles from "../styles.module.scss";
 import classNames from "classnames";
 // import { IPopupContext, PopupContext } from "../../../context/PopupContext";
