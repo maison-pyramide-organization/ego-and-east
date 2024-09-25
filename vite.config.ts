1053ff3ef5d9c07f0ecf786d6b4ca8ad43dc8b09
Mohamed Sameh <mxsameh@gmail.com>
2024-09-25 10:35:39 +0300

    sync

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import autoprefixer from "autoprefixer";
+import postcssPresetEnv from "postcss-preset-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-})
+  // Post Css
+  css: {
+    postcss: {
+      plugins: [autoprefixer, postcssPresetEnv],
+    },
+  },
+  // Server
+  server: {
+    host: true,
+  },
+});
