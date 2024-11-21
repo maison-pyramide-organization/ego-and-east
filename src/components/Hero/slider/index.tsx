12dd964e5e3247a70004f366ab81ad8050411fb6
Mohamed Sameh <mxsameh@gmail.com>
2024-11-21 13:03:14 +0200

    sync

--- src/components/Hero/slider/index.tsx
+++ src/components/Hero/slider/index.tsx
@@ -1,41 +1,54 @@
-import styles from "./styles.module.scss";
+import s from "./styles.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Autoplay } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
+import { ReactComponent as DownChevI } from "../../../assets/Icons/down chevron.svg";
 
 interface Iprops {
-    slides: any;
+  slides: any;
 }
 const Slider = (props: Iprops) => {
-    const { slides } = props;
-    if (!slides) return null;
-    return (
-        <Swiper
-            modules={[Autoplay]}
-            loop={true}
-            autoplay={{ delay: 8000 }}
-            slidesPerView={1}
-            breakpoints={{
-                1024: {
-                    slidesPerView: 3,
-                    spaceBetween: 64,
-                },
-            }}
-        >
-            {slides.map((slide: any, i: number) => (
-                <SwiperSlide className={styles.slide} key={i}>
-                    <div className={styles.slide_img_wrapper}>
-                        <img
-                            src={slide.url + "?w=1000"}
-                            className={styles.slide_img}
-                            alt={slide.title}
-                        />
-                    </div>
-                    <span className={styles.slide_title}>{slide.title}</span>
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    );
+  const { slides } = props;
+  if (!slides) return null;
+  return (
+    <div className={s.swiper_}>
+      <nav className={s.slider_nav}>
+        <button id="slider-prv" type="button">
+          <DownChevI />
+        </button>
+        <button id="slider-next" type="button">
+          <DownChevI />
+        </button>
+      </nav>
+
+      <Swiper
+        modules={[Autoplay, Navigation]}
+        loop={true}
+        autoplay={{ delay: 8000 }}
+        slidesPerView={1}
+        navigation={{ nextEl: "#slider-next", prevEl: "#slider-prev" }}
+        breakpoints={{
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 64,
+          },
+        }}
+      >
+        {slides.map((slide: any, i: number) => (
+          <SwiperSlide className={s.slide} key={i}>
+            <div className={s.slide_img_wrapper}>
+              <img
+                src={slide.url + "?w=1000"}
+                className={s.slide_img}
+                alt={slide.title}
+              />
+            </div>
+            <span className={s.slide_title}>{slide.title}</span>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Slider;
