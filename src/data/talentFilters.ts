f6ed943b9879430d7e02e43591a55ba0daacd1ba
Mohamed Sameh <mxsameh@gmail.com>
2025-10-20 11:18:34 +0300

    sync

--- src/data/talentFilters.ts
+++
@@ -1,8 +0,0 @@
-const filters = [
-  { name: "all", category: "all" },
-  { name: "actors", category: "actor" },
-  { name: "creatives", category: "creative" },
-  { name: "musicians", category: "musician" },
-];
-
-export default filters;
