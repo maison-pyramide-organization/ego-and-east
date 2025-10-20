5884933d3e4cf4f41fcaf6fd8fae983eb99d2680
Mohamed Sameh <mxsameh@gmail.com>
2025-10-20 11:11:26 +0300

    sync

--- ee/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import slide from './slide'
-import service from './service'
-import playlist from './playlist'
-import talent from './talent'
-
-export const schemaTypes = [slide, service, playlist, talent]
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import "./assets/index.css";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
-import PopupProvider from "./context/PopupContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <PopupProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  </PopupProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
 );
