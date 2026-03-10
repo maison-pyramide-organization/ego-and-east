7b45f40a2b7aa73b89a64baa70ed90ebc158da3b
Mohamed Sameh <mxsameh@gmail.com>
2026-03-10 04:37:18 +0200

    sync

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import "@s/index.css";
 import "@s/utils.css";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Talent from "./pages/talents/[slug]";
+import Home from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/landing" element={<Landing />} />
       <Route path="/talents/:slug" element={<Talent />} />
     </Routes>
   </BrowserRouter>,
---
+++ src/pages/Home/c/campaigns/index.tsx
@@ -0,0 +1,25 @@
+import campaigns from "@/data/campaigns";
+import s from "./_s.module.scss";
+
+export default function Campaigns() {
+  return (
+    <section className={s.s}>
+      <ul>
+        {campaigns.map((campaign) => (
+          <li className={s.campaign}>
+            <figure>
+              <img
+                src={campaign.img}
+                alt={`${campaign.talent} - ${campaign.brand}`}
+              />
+            </figure>
+            <h4>
+              {campaign.talent}
+              <span>for {campaign.brand}</span>
+            </h4>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
