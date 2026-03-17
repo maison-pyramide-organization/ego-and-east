1613e9551dd68bdcabc18d6e0abf8250303f35be
Mohamed Sameh <mxsameh@gmail.com>
2026-03-17 06:05:26 +0200

    sync

--- src/pages/Home/c/hero/index.tsx
+++ src/pages/Home/c/hero/index.tsx
@@ -2,25 +2,32 @@ import s from "./_s.module.scss";
 
 export default function Hero() {
   return (
-    <section id="he-s" className={s.s}>
-      <div id="hero" className={s.hero}>
-        <figure id="vid">
+    <>
+      <main className={s.m}>
+        <figure>
           <video src="/vids/ego-demo.mp4" autoPlay playsInline loop muted />
         </figure>
-        <p>
-          Co-founders Reem and Natalya Kanj have propelled Ego & East into one
-          of the most prominent
-        </p>
-        <p>
-          The Lebanese sisters and business partners have crafted an innovative
-          approach to talent management,
-        </p>
-        <h1>
-          We connect top global luxury, beauty and fashion brands with the most
-          compelling celebrities, actors, musicians, creatives and influencers
-          in the Middle East – and beyond.
-        </h1>
-      </div>
-    </section>
+      </main>
+      <section id="he-s" className={s.s}>
+        <div id="hero" className={s.hero}>
+          <figure id="vid">
+            <video src="/vids/ego-demo.mp4" autoPlay playsInline loop muted />
+          </figure>
+          <p>
+            Co-founders Reem and Natalya Kanj have propelled Ego & East into one
+            of the most prominent
+          </p>
+          <p>
+            The Lebanese sisters and business partners have crafted an
+            innovative approach to talent management,
+          </p>
+          <h1>
+            We connect top global luxury, beauty and fashion brands with the
+            most compelling celebrities, actors, musicians, creatives and
+            influencers in the Middle East – and beyond.
+          </h1>
+        </div>
+      </section>
+    </>
   );
 }
