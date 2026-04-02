671530d4b6749a9aba542d4e87633ef29805cb09
Mohamed Sameh <mxsameh@gmail.com>
2026-04-02 12:55:00 +0200

    sync

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -17,39 +17,24 @@ export default function About() {
                 Their innovative approach to talent management, influenced by
                 their extensive experience in London and the MENA region, has
                 solidified Ego & East as a leader in Middle East and GCC talent
-                management, particularly within the KSA. Ego & East connects top
-                luxury, beauty, and fashion brands with the most compelling
-                talent across the MENA region, including influencers,
-                celebrities, actors, musicians, and creatives. As a celebrity
-                and brand ambassador management agency, they specialize in
-                elevating brands through influencer marketing and strategic
-                talent partnerships.
+                management, particularly within the KSA.
                 <br />
                 <br />
-                Their distinctively mindful – yet tenacious – approach to brand
-                elevation has earned them
-                <span className="m-o">
-                  features in leading publications such as Vogue Business,
-                  Emirates Woman, Vogue Arabia, Grazia, and Cosmopolitan.
-                  Additionally, they have been recognized for their expertise in
-                  fashion week services and invited to guest lecture at Istituto
-                  Marangoni, one of the world's most prestigious fashion
-                  schools.
-                  <br />
-                  <br />
-                  Recently, Ego & East embarked on an exciting new venture,
-                  merging with the leading agency Maison Pyramide. This
-                  partnership is set to become ‘one of the most forward-thinking
-                  mergers in the talent management and influencer marketing
-                  industry’.
-                </span>
+                Ego & East connects top luxury, beauty, and fashion brands with
+                the most compelling talent across the MENA region, including
+                influencers, celebrities, actors, musicians, and creatives. As a
+                celebrity and brand ambassador management agency, they
+                specialize in elevating brands through influencer marketing and
+                strategic talent partnerships.
               </p>
-              <p className="d-o">
-                features in leading publications such as Vogue Business,
-                Emirates Woman, Vogue Arabia, Grazia, and Cosmopolitan.
-                Additionally, they have been recognized for their expertise in
-                fashion week services and invited to guest lecture at Istituto
-                Marangoni, one of the world's most prestigious fashion schools.
+              <p >
+                Their distinctively mindful – yet tenacious – approach to brand
+                elevation has earned them features in leading publications such
+                as Vogue Business, Emirates Woman, Vogue Arabia, Grazia, and
+                Cosmopolitan. Additionally, they have been recognized for their
+                expertise in fashion week services and invited to guest lecture
+                at Istituto Marangoni, one of the world's most prestigious
+                fashion schools.
                 <br />
                 <br />
                 Recently, Ego & East embarked on an exciting new venture,
